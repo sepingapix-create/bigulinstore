0f1d4fe500d11bc640dd4dd07bd2d1e6368c6578
joao vt <sepingapix@gmail.com>
2026-04-25 17:25:11 -0300

    update

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { Search, ShoppingCart, User, LogOut, LayoutDashboard, Users } from "lucide-react";
+import { Search, ShoppingCart, User, LogOut, LayoutDashboard, Users, Zap } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
